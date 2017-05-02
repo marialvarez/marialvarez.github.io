eaef333d50846e65cd89970357294d1afbff452c
Maricela <malvarezdrobush@gmail.com>
2017-05-02 14:19:53 -0500

    menu responsive changed

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
     
-    $(this).scrollTop(0);
+    /*$(this).scrollTop(0);
     
     // Add scrollspy to <body>
     $('body').scrollspy({target: ".navbar", offset: 50});   
@@ -33,6 +33,44 @@ $(document).ready(function(){
           $(this).removeClass("hover");
         }
       );
+      
+      */
+            // Sticky Header
+        $(window).scroll(function() {
+
+            if ($(window).scrollTop() > 100) {
+                $('.main_h').addClass('sticky');
+            } else {
+                $('.main_h').removeClass('sticky');
+            }
+        });
+
+        // Mobile Navigation
+        $('.mobile-toggle').click(function() {
+            if ($('.main_h').hasClass('open-nav')) {
+                $('.main_h').removeClass('open-nav');
+            } else {
+                $('.main_h').addClass('open-nav');
+            }
+        });
+
+        $('.main_h li a').click(function() {
+            if ($('.main_h').hasClass('open-nav')) {
+                $('.navigation').removeClass('open-nav');
+                $('.main_h').removeClass('open-nav');
+            }
+        });
+
+        // Navigation Scroll - ljepo radi materem
+        $('nav a').click(function(event) {
+            var id = $(this).attr("href");
+            var offset = 70;
+            var target = $(id).offset().top - offset;
+            $('html, body').animate({
+                scrollTop: target
+            }, 500);
+            event.preventDefault();
+        });
 });
 
 /*---------SCROLL----------*/
